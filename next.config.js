7d4c62e39bc4b3c5672ab2a763e8628d791d1a77
DONG-PC <choise154@gmail.com>
2022-01-16 13:15:49 +0900

    next.config.js 다루는법, .env 다루는 법

--- next.config.js
+++ next.config.js
@@ -1,3 +1,22 @@
+const API_KEY = process.env.API_KEY
+
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/test1/:path*',
+        destination: "test2/:path*",
+        permanent: false
+      }
+    ]
+  },
+  async rewrites () {
+    return [
+      {
+        source: '/api/test',
+        destination: `https://api호스트주소?api_key=${API_KEY}`
+      }
+    ]
+  }
 }
