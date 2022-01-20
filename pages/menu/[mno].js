b17a9d39001a6e2aa2de909d2c7767cd91ec9252
DONG-PC <choise154@gmail.com>
2022-01-20 23:53:21 +0900

    list -> collapse로 변경

--- pages/menu/[mno].js
+++
@@ -1,8 +0,0 @@
-export default function menuDetail({ mno }) {
-    console.log(mno)
-    return (
-        <>
-           <h2>menu detail</h2> 
-        </>
-    );
-}
--- pages/menu/index.js
+++ pages/menu/index.js
@@ -1,35 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-import { List, Skeleton } from 'antd';
+import { Collapse, Skeleton } from 'antd';
+import { CaretRightOutlined } from '@ant-design/icons';
 import DLoadMore from '../../components/DLoadMore';
-
-export default function test() {
+const { Panel } = Collapse;
+export default function test({ testcase }) {
     const [data, setData] = useState([]);
     const [list, setList] = useState([]);
     const [initLoading, setInitLoading] = useState(true);
     const [loading, setLoading] = useState(true);
-    const testcase = [
-        {
-            no: 1,
-            title: '2022년 1월 셋째주(17-21) 식단',
-            description: '벌써~!'
-        },
-        { 
-            no: 2,
-            title: '2022년 1월 둘째주(10-14) 식단',
-            description: '벌써~!'
-        },
-        { 
-            no: 3,
-            title: '2022년 1월 첫째주(3-7) 식단',
-            description: null
-        },
-        { 
-            no: 4,
-            title: '2021년 12월 셋째주(27-31) 식단',
-            description: '벌써~!'
-        }
-    ]
+    
     useEffect( () => {
         setTimeout( () => {
             setInitLoading(false);
@@ -54,30 +34,47 @@ export default function test() {
     const loadMore = !initLoading && !loading ? <DLoadMore loadFn={onLoadMore}>더보기</DLoadMore>: null;
     return (
         <>
-            <List
-                bordered
-                style={{border: 0, backgroundColor: '#ffffff'}}
-                loading={initLoading}
-                dataSource={list}
-                loadMore={loadMore}
-                renderItem={item => (
-                    <List.Item
-                        style={{backgroundColor: '#ffffff'}}>
-                            <Skeleton title={false} loading={item.loading} active>
-                                <Link href={`/menu/${item.no}`}>
-                                    <List.Item.Meta
-                                        title={item.title}
-                                        description={item.description}
-                                    />
-                                </Link>
-                                
-                            </Skeleton>
-                    </List.Item>
-                )}
-            />
-            <style jsx>{`
-
-            `}</style>
+            <Collapse
+                bordered={false}
+                expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />}
+                className="site-collapse-custom-collapse"
+            >
+                {testcase?.map((item) => (
+                    <Panel header={item.title} key={item.no} className="site-collapse-custom-panel">
+                        <p>{'text'}</p>
+                    </Panel>
+                ))}
+            </Collapse>
+            <loadMore/>
         </>
     );
+}
+
+export async function getServerSideProps() {
+    return {
+        props: {
+            testcase : [
+                {
+                    no: 1,
+                    title: '2022년 1월 셋째주(17-21) 식단',
+                    description: '벌써~!'
+                },
+                { 
+                    no: 2,
+                    title: '2022년 1월 둘째주(10-14) 식단',
+                    description: '벌써~!'
+                },
+                { 
+                    no: 3,
+                    title: '2022년 1월 첫째주(3-7) 식단',
+                    description: null
+                },
+                { 
+                    no: 4,
+                    title: '2021년 12월 셋째주(27-31) 식단',
+                    description: '벌써~!'
+                }
+            ]
+        }
+    }
 }
