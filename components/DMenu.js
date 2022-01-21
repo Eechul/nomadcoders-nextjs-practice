e748e27bb138c38d13dca40fefd4950ee563317d
Dongd <choise154@gmail.com>
2022-01-21 18:03:02 +0900

    메뉴 인라인 정리

--- components/DMenu.js
+++ components/DMenu.js
@@ -5,49 +5,61 @@ export default function DMenul() {
     return (
         <>
             <Row gutter={8}>
-                    <Col span={12}>
-                        <Card>
-                            <p className="week">월 </p>
-                            <DFood tooltip={'면역력 up'}>돈까스</DFood>
-                            <DFood>감자볶음</DFood>
-                            <DFood>김치</DFood>
-                            <DFood>무피클</DFood>
-                            <DFood>미역국</DFood>
-                        </Card>
-                    </Col>
-                    <Col span={12}>
-                        <Card>
-                            <p className="week">목</p>
-                            <DFood tooltip={'면역력 up😆'}>돈까스</DFood>
-                            <DFood>감자볶음</DFood>
-                            <DFood>김치</DFood>
-                            <DFood>무피클</DFood>
-                            <DFood>미역국</DFood>
-                        </Card>
-                    </Col>
-                </Row>
-                <Row gutter={8}>
-                    <Col span={12}>
-                        <Card>
-                            <p className="week">월 </p>
-                            <DFood tooltip={'면역력 up'}>돈까스</DFood>
-                            <DFood>감자볶음</DFood>
-                            <DFood tooltip={'직접 담궜으😖'}>김치</DFood>
-                            <DFood>무피클</DFood>
-                            <DFood>미역국</DFood>
-                        </Card>
-                    </Col>
-                    <Col span={12}>
-                        <Card>
-                            <p className="week">목</p>
-                            <DFood tooltip={'면역력 up'}>돈까스</DFood>
-                            <DFood>감자볶음</DFood>
-                            <DFood>김치</DFood>
-                            <DFood tooltip={'😖조금 시어요😖'}>무피클</DFood>
-                            <DFood>미역국</DFood>
-                        </Card>
-                    </Col>
-                </Row>
-            </>
+                <Col span={12}>
+                    <Card>
+                        <p className="week">월 </p>
+                        <DFood tooltip={'면역력 up'}>돈까스</DFood>
+                        <DFood>감자볶음</DFood>
+                        <DFood>김치</DFood>
+                        <DFood>무피클</DFood>
+                        <DFood>미역국</DFood>
+                    </Card>
+                </Col>
+                <Col span={12}>
+                    <Card>
+                        <p className="week">화</p>
+                        <DFood tooltip={'면역력 up😆'}>돈까스</DFood>
+                        <DFood>감자볶음</DFood>
+                        <DFood>김치</DFood>
+                        <DFood>무피클</DFood>
+                        <DFood>미역국</DFood>
+                    </Card>
+                </Col>
+            </Row>
+            <Row gutter={8}>
+                <Col span={12}>
+                    <Card>
+                        <p className="week">수</p>
+                        <DFood tooltip={'면역력 up'}>돈까스</DFood>
+                        <DFood>감자볶음</DFood>
+                        <DFood tooltip={'직접 담궜으😖'}>김치</DFood>
+                        <DFood>무피클</DFood>
+                        <DFood>미역국</DFood>
+                    </Card>
+                </Col>
+                <Col span={12}>
+                    <Card>
+                        <p className="week">목</p>
+                        <DFood tooltip={'면역력 up'}>돈까스</DFood>
+                        <DFood>감자볶음</DFood>
+                        <DFood>김치</DFood>
+                        <DFood tooltip={'😖조금 시어요😖'}>무피클</DFood>
+                        <DFood>미역국</DFood>
+                    </Card>
+                </Col>
+            </Row>
+            <Row gutter={8}>
+                <Col span={12}>
+                    <Card>
+                        <p className="week">금</p>
+                        <DFood tooltip={'면역력 up'}>돈까스</DFood>
+                        <DFood>감자볶음</DFood>
+                        <DFood tooltip={'직접 담궜으😖'}>김치</DFood>
+                        <DFood>무피클</DFood>
+                        <DFood>미역국</DFood>
+                    </Card>
+                </Col>
+            </Row>
+        </>
     )
 }
