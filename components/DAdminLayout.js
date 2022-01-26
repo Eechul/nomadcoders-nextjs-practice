8a37282f33464b2a07fab1026ab31c139912b85b
DONG-PC <choise154@gmail.com>
2022-01-26 23:57:27 +0900

    admin 페이지 설계

--- components/DAdminLayout.js
+++ components/DAdminLayout.js
@@ -1,22 +1,49 @@
 import Head from "next/head";
-import Layout from "antd/lib/layout/layout";
-import { Row, Col } from 'antd';
-import NavBar from "./NavBar"
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
 export default function DLayout({ children }) {
 
     return (
-        <Layout>
-            <Head>
-                <title>title 어드민</title>
-            </Head>
-            <Row>
-                <Col span={24}>
-                    <NavBar />
-                    <div>{children}</div>
-                </Col>
-            </Row>
+        <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={false}>
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1" icon={<PieChartOutlined />}>
+              대시보드
+            </Menu.Item>
+            <Menu.Item key="2" icon={<DesktopOutlined />}>
+              게시판
+            </Menu.Item>
+            <Menu.Item key="3" icon={<FileOutlined />}>
+              공지사항
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+            {children}
+          <Footer style={{ textAlign: 'center' }}>©2022</Footer>
         </Layout>
+        <style jsx>{`
+            .logo {
+                height: 32px;
+                margin: 16px;
+                background: rgba(255, 255, 255, 0.3);
+            }
+            .site-layout .site-layout-background {
+                background: #fff;
+            }
+        `}</style>
+      </Layout>
         
     );
 }
---
+++ pages/admin/index.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { Collapse, Skeleton } from 'antd';
+import { CaretRightOutlined } from '@ant-design/icons';
+import DLoadMore from '../../components/DLoadMore';
+import DMenu from '../../components/DMenu';
+
+export default function Index() {
+    
+    return (
+        <>
+            <div>gdgd</div>
+        </>
+    );
+}
